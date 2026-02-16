e8e25d598246e38091aeee9abc28fdfe1c20a195
Arun Raghunath <arun2aruns@gmail.com>
2026-02-16 00:44:33 +0000

    feat: Path Resolver changes and tests completed

--- inode_fs/path_resolver.py
+++ inode_fs/path_resolver.py
@@ -1,3 +1,8 @@
+from inode_fs.errors import NotADirectoryError
+from inode_fs.inode import DirInode, Inode
+from inode_fs.inode_table import InodeTable
+
+
 def normalize(path: str) -> str:
     path_stack: list[str] = []
     path_list = path.split("/")
@@ -10,3 +15,24 @@ def normalize(path: str) -> str:
         else:
             path_stack.append(p.strip())
     return "/" + "/".join(path_stack)
+
+
+def resolve(path: str, inode_table: InodeTable) -> Inode:
+    path = normalize(path)
+    path_list: list[str] = path.split("/")
+    path_list = [p for p in path_list if p]
+    cur_inode = inode_table.get(0)
+    for i in range(0, len(path_list) - 1):
+        if not isinstance(cur_inode, DirInode):
+            raise NotADirectoryError(f"[inode-fs] Path {path} is not a directory")
+        cur_inode_id = cur_inode.get_entry(path_list[i])
+        cur_inode = inode_table.get(cur_inode_id)
+
+    if path_list:
+        if not isinstance(cur_inode, DirInode):
+            raise NotADirectoryError(f"[inode-fs] Path {path} is not a directory")
+
+        cur_inode_id = cur_inode.get_entry(path_list[-1])  #
+        cur_inode = inode_table.get(cur_inode_id)
+
+    return cur_inode
--- tests/test_path_resolver.py
+++ tests/test_path_resolver.py
@@ -1,38 +1,108 @@
+from inode_fs.path_resolver import normalize, resolve
+from inode_fs.inode_table import InodeTable
+from inode_fs.inode import Inode, DirInode, FileInode, _reset_counter
+from inode_fs.errors import NotFoundError, NotADirectoryError
 import pytest
 
-from inode_fs.path_resolver import normalize
+class TestNormalize:
+    def test_full_path(self) -> None:
+        _reset_counter()
+        path = "/home/file/test.txt"
+        response = normalize(path)
+        assert response == "/home/file/test.txt"
 
-def test_full_path() -> None:
-    path = "/home/file/test.txt"
-    response = normalize(path)
-    assert response == "/home/file/test.txt"
 
-def test_ignore_current_dir() -> None:
-    path = "/home/./file/test.txt"
-    response = normalize(path)
-    assert response == "/home/file/test.txt"
+    def test_ignore_current_dir(self) -> None:
+        _reset_counter()
+        path = "/home/./file/test.txt"
+        response = normalize(path)
+        assert response == "/home/file/test.txt"
 
-def test_parent_dir() -> None:
-    path = "/home/../file/test.txt"
-    response = normalize(path)
-    assert response == "/file/test.txt"
 
-def test_past_root() -> None:
-    path = "../home/file/test.txt"
-    response = normalize(path)
-    assert response == "/home/file/test.txt"
+    def test_parent_dir(self) -> None:
+        _reset_counter()
+        path = "/home/../file/test.txt"
+        response = normalize(path)
+        assert response == "/file/test.txt"
 
-def test_ignore_double_slash() -> None:
-    path = "//home//file//test.txt"
-    response = normalize(path)
-    assert response == "/home/file/test.txt"
 
-def test_ignore_trailing_slash() -> None:
-    path = "/home/file/test.txt/"
-    response = normalize(path)
-    assert response == "/home/file/test.txt"
+    def test_past_root(self) -> None:
+        _reset_counter()
+        path = "../home/file/test.txt"
+        response = normalize(path)
+        assert response == "/home/file/test.txt"
 
-def test_root_only() -> None:
-    path = "/"
-    response = normalize(path)
-    assert response == "/"
+
+    def test_ignore_double_slash(self) -> None:
+        _reset_counter()
+        path = "//home//file//test.txt"
+        response = normalize(path)
+        assert response == "/home/file/test.txt"
+
+
+    def test_ignore_trailing_slash(self) -> None:
+        _reset_counter()
+        path = "/home/file/test.txt/"
+        response = normalize(path)
+        assert response == "/home/file/test.txt"
+
+
+    def test_root_only(self) -> None:
+        _reset_counter()
+        path = "/"
+        response = normalize(path)
+        assert response == "/"
+
+class TestResolve:
+    @pytest.fixture
+    def get_setup(self) -> InodeTable:
+        _reset_counter()
+        inode_table = InodeTable()
+        docs_inode = DirInode()
+        report_inode = FileInode()
+        docs_inode.add_entry("report.txt", report_inode.inode_id)
+        file_inode = FileInode()
+        root_inode = inode_table.get(0)
+        if isinstance(root_inode, DirInode):
+            root_inode.add_entry("docs", docs_inode.inode_id)
+            root_inode.add_entry("file.txt", file_inode.inode_id)
+        inode_table.add(docs_inode)
+        inode_table.add(report_inode)
+        inode_table.add(file_inode)
+
+        return inode_table
+
+    def test_root_only(self, get_setup: InodeTable) -> None:
+        _reset_counter()
+        inode_table = get_setup
+        path = "/"
+        root_node = resolve(path, inode_table)
+        assert root_node == inode_table.get(0)
+
+    def test_directory_path(self, get_setup: InodeTable) -> None:
+        _reset_counter()
+        inode_table = get_setup
+        path = "/docs"
+        docs_node = resolve(path, inode_table)
+        assert isinstance(docs_node, DirInode)
+
+    def test_file_path(self, get_setup: InodeTable) -> None:
+        _reset_counter()
+        inode_table = get_setup
+        path = "/docs/report.txt"
+        reports_node = resolve(path, inode_table)
+        assert isinstance(reports_node, FileInode)
+
+    def test_missing_directory(self, get_setup: InodeTable) -> None:
+        _reset_counter()
+        inode_table = get_setup
+        path = "/missing"
+        with pytest.raises(NotFoundError):
+            missing_node = resolve(path, inode_table)
+
+    def test_not_a_directory(self, get_setup: InodeTable) -> None:
+        _reset_counter()
+        inode_table = get_setup
+        path = "/file.txt/docs"
+        with pytest.raises(NotADirectoryError):
+            error_node = resolve(path, inode_table)
